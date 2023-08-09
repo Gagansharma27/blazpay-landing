cf5d3c9f3a43df5e488bfb52ef36485166c780f8
Gagan Sharma <sharmagagan1820@gmail.com>
2023-08-09 17:47:31 +0530

    initialized

---
+++ src/utilities/Gradient.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Gradient = () => {
+  return (
+    <div
+      className={`w-[100px] h-[100px] bg-gradient-to-r from-[#FF3503] to-yellow-500 rounded-[10%]`}
+    ></div>
+  );
+};
+
+export default Gradient;
